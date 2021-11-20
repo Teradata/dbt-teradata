157ab7d2488a150aefdb147add4c2165aeeca686
Doug Beatty <doug.beatty@gmail.com>
2021-11-19 18:20:28 -0700

    Stored procedures for dropping tables and views

---
+++ script/dbt_drop_relation_if_exists.sql
@@ -0,0 +1,22 @@
+
+replace procedure dbt_test.dbt_drop_relation_if_exists(
+  IN relation_type varchar(10),
+  IN full_name varchar(256)
+)
+begin
+  DECLARE sql_stmt VARCHAR(500)  CHARACTER SET Unicode;
+  DECLARE msg VARCHAR(400) CHARACTER SET Unicode;
+
+  DECLARE exit HANDLER FOR SqlException
+  BEGIN
+    IF SqlCode = 3807 THEN SET msg = full_name || ' does not exist.';
+    ELSEIF SqlCode = 3853 THEN SET msg = full_name || ' is not a table.';
+    ELSEIF SqlCode = 3854 THEN SET msg = full_name || ' is not a view.';
+    ELSE
+      RESIGNAL;
+    END IF;
+  END;
+
+  SET sql_stmt = 'DROP ' || relation_type || ' ' || full_name || ';';
+  EXECUTE IMMEDIATE sql_stmt;
+END;
