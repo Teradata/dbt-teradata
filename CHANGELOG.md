9e4b959082e42620177ca50665539a9d55dc3b7e
Adam Tworkiewicz <adam.tworkiewicz@teradata.com>
2022-05-10 10:33:25 -0500

    upgrade to dbt 1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 ## dbt-teradata 1.0.0a
 
 ### Features
-* Upgraded to dbt 1.0.2
+* Upgraded to dbt 1.0.3
 
 ### Fixes
 
--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         ],
     },
     install_requires=[
-        "dbt-core==1.0.2",
+        "dbt-core==1.0.3",
         "teradatasql>=16.20.0.0",
     ],
     classifiers=[
