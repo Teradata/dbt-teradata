cd49ca1f6bf0ed903e587da206d3b30f73dfb914
Adam Tworkiewicz <adamtwo@users.noreply.github.com>
2022-05-10 17:46:46 -0500

    Upgraded to 1.0.5 (#41)
    Co-authored-by: Adam Tworkiewicz <adam.tworkiewicz@teradata.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 ## dbt-teradata 1.0.0a
 
 ### Features
-
+* Upgrade to dbt 1.0.5
 ### Fixes
 
 ### Docs
--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         ],
     },
     install_requires=[
-        "dbt-core==1.0.4",
+        "dbt-core==1.0.5",
         "teradatasql>=16.20.0.0",
     ],
     classifiers=[
