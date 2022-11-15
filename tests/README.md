c0318e31a16b23a2a58fd41a5419fc1ca48f65f4
vs255034 <varun1598sharma@gmail.com>
2022-11-15 21:53:05 +0530

    altered the conftest.py file to remove hard coding of system configuration

--- tests/README.md
+++ tests/README.md
@@ -15,7 +15,12 @@ pip install -r requirements_dev.txt
 ```
 
 Here are the steps to run the functional tests:
-1. Set up the server in file conftest.py, provide server configurations like
-   hostname, username, password etc.
+1. set the environment variables related to the system cofiguration for e.g :
+   export DBT_TERADATA_SERVER_NAME='sdt****.labs.teradata.com or server_ip'
+   export DBT_TERADATA_USERNAME='***'
+   export DBT_TERADATA_PASSWORD='***'
+   
+   If there are no environment variables provided than the default values will be picked which could be found in conftest.py file.
+   
 2. Run the test with python pytest command, for e.g  
    python -m pytest .\tests\functional\adapter\test_basic.py                                         
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,35 @@
 import pytest
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv, find_dotenv
+#from pathlib import Path
 
-load_dotenv()
+#load_dotenv("../test.env")
 
 # Import the standard functional fixtures as a plugin
-# Note: fixtures with session scope need to be local
+# Note: fixtures with session scope need to be locals
 pytest_plugins = ["dbt.tests.fixtures.project"]
 
 # The profile dictionary, used to write out profiles.yml
 # dbt will supply a unique schema per test, so we do not specify 'schema' here
 @pytest.fixture(scope="class")
 def dbt_profile_target():
+    hostname='localhost'
+    username='dbc'
+    password='dbc'
+    if os.getenv('DBT_TERADATA_SERVER_NAME'):
+        hostname=os.getenv('DBT_TERADATA_SERVER_NAME')
+    
+    if os.getenv('DBT_TERADATA_USERNAME'):
+        username=os.getenv('DBT_TERADATA_USERNAME')
+    
+    if os.getenv('DBT_TERADATA_PASSWORD'):
+        password=os.getenv('DBT_TERADATA_PASSWORD')
+    
     return {
         'type': 'teradata',
         'threads': 1,
-        'host': 'localhost',
-        'user': 'dbc',
-        'password': 'dbc'
-        
+        'host': hostname, #{{os.getenv('HOST'), 'localhost'}},
+        'user': username, #{{os.getenv('USER'), 'dbc'}},
+        'password': password, #{{os.getenv('PASSWORD'), 'dbc'}},
+        'schema': "dbt_test_{{ var('_dbt_random_suffix') }}"
     }
