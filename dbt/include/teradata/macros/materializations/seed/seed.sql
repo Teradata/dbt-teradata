194e999402a569fb5cf047bdf9354f6d5949c4cf
Talla <mohan.talla@teradata.com>
2023-01-24 16:24:15 +0530

    apply grants for seed

--- dbt/include/teradata/macros/materializations/seed/seed.sql
+++ dbt/include/teradata/macros/materializations/seed/seed.sql
@@ -94,6 +94,11 @@
 
   {{ run_hooks(post_hooks, inside_transaction=True) }}
 
+  -- Apply grants
+  {% set grant_config = config.get('grants') %}
+  {% set should_revoke = should_revoke(old_relation, full_refresh_mode) %}
+  {% do apply_grants(target_relation, grant_config, should_revoke) %}
+
   -- `COMMIT` happens here (for tmode=ANSI)
   {{ adapter.commit() }}
 
