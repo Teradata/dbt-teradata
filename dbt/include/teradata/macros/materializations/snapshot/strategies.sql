5040d3a7659d59671bd8d0208fcfa4c956d644d6
Septimiu Turcu <septimiu.turcu@teradata.com>
2023-11-01 07:50:10 +0100

    Update snapshot code to dbt-core 1.6.2 (#115)

--- dbt/include/teradata/macros/materializations/snapshot/strategies.sql
+++ dbt/include/teradata/macros/materializations/snapshot/strategies.sql
@@ -1,7 +1,90 @@
 
 {% macro teradata__snapshot_hash_arguments(args) -%}
     HASHROW({%- for arg in args -%}
-        coalesce(cast({{ arg }} AS VARCHAR(50) ), '')
+        coalesce(cast({{ arg }} as varchar(50)), '')
         {% if not loop.last %} || '|' || {% endif %}
     {%- endfor -%})
 {%- endmacro %}
+
+{% macro snapshot_check_all_get_existing_columns(node, target_exists, check_cols_config) -%}
+    {%- if not target_exists -%}
+        {#-- no table yet -> return whatever the query does --#}
+        {{ return((false, query_columns)) }}
+    {%- endif -%}
+
+    {#-- handle any schema changes --#}
+    {%- set target_relation = adapter.get_relation(database=None, schema=node.schema, identifier=node.alias) -%}
+
+    {% if check_cols_config == 'all' %}
+        {%- set query_columns = get_columns_in_query(node['compiled_code']) -%}
+
+    {% elif check_cols_config is iterable and (check_cols_config | length) > 0 %}
+        {#-- query for proper casing/quoting, to support comparison below --#}
+        {%- set select_check_cols_from_target -%}
+            {#-- N.B. The whitespace below is necessary to avoid edge case issue with comments --#}
+            {#-- See: https://github.com/dbt-labs/dbt-core/issues/6781 --#}
+            select {{ check_cols_config | join(', ') }} from (
+                {{ node['compiled_code'] }}
+            ) subq
+        {%- endset -%}
+        {% set query_columns = get_columns_in_query(select_check_cols_from_target) %}
+
+    {% else %}
+        {% do exceptions.raise_compiler_error("Invalid value for 'check_cols': " ~ check_cols_config) %}
+    {% endif %}
+
+    {%- set existing_cols = adapter.get_columns_in_relation(target_relation) | map(attribute = 'name') | list -%}
+    {%- set ns = namespace() -%} {#-- handle for-loop scoping with a namespace --#}
+    {%- set ns.column_added = false -%}
+
+    {%- set intersection = [] -%}
+    {%- for col in query_columns -%}
+        {%- if col in existing_cols -%}
+            {%- do intersection.append(adapter.quote(col)) -%}
+        {%- else -%}
+            {% set ns.column_added = true %}
+        {%- endif -%}
+    {%- endfor -%}
+    {{ return((ns.column_added, intersection)) }}
+{%- endmacro %}
+
+{#-- This macro is copied varbatim from dbt-core. The only delta is that != operator is replaced with <> #}
+{% macro snapshot_check_strategy(node, snapshotted_rel, current_rel, config, target_exists) %}
+    {% set check_cols_config = config['check_cols'] %}
+    {% set primary_key = config['unique_key'] %}
+    {% set invalidate_hard_deletes = config.get('invalidate_hard_deletes', false) %}
+    {% set updated_at = config.get('updated_at', snapshot_get_time()) %}
+
+    {% set column_added = false %}
+
+    {% set column_added, check_cols = snapshot_check_all_get_existing_columns(node, target_exists, check_cols_config) %}
+
+    {%- set row_changed_expr -%}
+    (
+    {%- if column_added -%}
+        {{ get_true_sql() }}
+    {%- else -%}
+    {%- for col in check_cols -%}
+        {{ snapshotted_rel }}.{{ col }} <> {{ current_rel }}.{{ col }}
+        or
+        (
+            (({{ snapshotted_rel }}.{{ col }} is null) and not ({{ current_rel }}.{{ col }} is null))
+            or
+            ((not {{ snapshotted_rel }}.{{ col }} is null) and ({{ current_rel }}.{{ col }} is null))
+        )
+        {%- if not loop.last %} or {% endif -%}
+    {%- endfor -%}
+    {%- endif -%}
+    )
+    {%- endset %}
+
+    {% set scd_id_expr = snapshot_hash_arguments([primary_key, updated_at]) %}
+
+    {% do return({
+        "unique_key": primary_key,
+        "updated_at": updated_at,
+        "row_changed": row_changed_expr,
+        "scd_id": scd_id_expr,
+        "invalidate_hard_deletes": invalidate_hard_deletes
+    }) %}
+{% endmacro %}
