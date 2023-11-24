95339c39667ebba04ab123dc35015749564150fd
Adam Tworkiewicz <adam.tworkiewicz@teradata.com>
2023-11-23 23:00:32 -0600

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ pytest_plugins = ["dbt.tests.fixtures.project"]
 
 # The profile dictionary, used to write out profiles.yml
 # dbt will supply a unique schema per test, so we do not specify 'schema' here
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="class")
 def dbt_profile_target():
     hostname='localhost'
     username='dbc'
