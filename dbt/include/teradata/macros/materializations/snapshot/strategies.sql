f2ecadbd635871e75e33e6250b5827d1ca6f92d9
Talla <mohan.talla@teradata.com>
2024-06-05 08:20:48 +0530

    Replacing HASHROW with GLOBAL_FUNCTIONS.hash_md5

--- dbt/include/teradata/macros/materializations/snapshot/strategies.sql
+++ dbt/include/teradata/macros/materializations/snapshot/strategies.sql
@@ -1,6 +1,6 @@
 
 {% macro teradata__snapshot_hash_arguments(args) -%}
-    HASHROW({%- for arg in args -%}
+    GLOBAL_FUNCTIONS.hash_md5({%- for arg in args -%}
         coalesce(cast({{ arg }} as varchar(50)), '')
         {% if not loop.last %} || '|' || {% endif %}
     {%- endfor -%})
