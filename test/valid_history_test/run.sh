7ab5ff5dd79a783932786f34d22b237f79b95dbc
Varun Sharma <varun1598sharma@gmail.com>
2024-10-09 11:56:53 +0530

    workflow changes

--- test/valid_history_test/run.sh
+++ test/valid_history_test/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+dbt -d deps --target dbt_test_valid_history
 dbt -d seed --target dbt_test_valid_history --full-refresh
+dbt -d run --target dbt_test_valid_history
 dbt -d run --target dbt_test_valid_history
