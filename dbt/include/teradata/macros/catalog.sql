5e6980e224d716b93a4fd2eb419bfa335637c1ba
Talla, Mohan <tallamohan@gmail.com>
2024-12-18 17:44:48 +0530

    Removed QVCI dependency in dbt-teradata

--- dbt/include/teradata/macros/catalog.sql
+++ dbt/include/teradata/macros/catalog.sql
@@ -1,7 +1,48 @@
+{% macro teradata__get_views_from_relations(relations) -%}
+    {% set view_relations = [] %}
+    {%- for relation in relations -%}
+        {% if relation.schema and relation.identifier %}
+            {% call statement('check_table_or_view', fetch_result=True) %}
+                SELECT TableKind FROM DBC.TablesV WHERE DatabaseName = '{{ relation.schema }}' AND TableName = '{{ relation.identifier }}'
+            {% endcall %}
+            {% set table_kind = load_result('check_table_or_view').table.columns['TableKind'].values()[0] %}
+            {%- if table_kind == 'V ' -%}
+                {% do view_relations.append(relation) %}
+            {%- endif -%}
+        {% endif %}
+    {%- endfor %}
+    {{ return(view_relations) }}
+{%- endmacro %}
+
+{% macro teradata__create_tmp_tables_of_views(view_relations) -%}
+    {% set view_tmp_tables_mapping = {} %}
+    {%- for relation in view_relations -%}
+        {% set temp_relation_for_view = relation.identifier ~ '_viw_tbl' %}
+        {% set view_tmp_tables_mapping = view_tmp_tables_mapping.update({relation: temp_relation_for_view}) %}
+        {% call statement('drop_existing_table', fetch_result=False) %}
+            DROP table /*+ IF EXISTS */ "{{ relation.schema }}"."{{ temp_relation_for_view }}";
+        {% endcall %}
+        load_result('drop_existing_table')
+        {% call statement('creating_table_from_view', fetch_result=False) %}
+            CREATE TABLE "{{ relation.schema }}"."{{ temp_relation_for_view }}" AS (SELECT * FROM "{{ relation.schema }}"."{{ relation.identifier }}") WITH NO DATA;
+        {% endcall %}
+        load_result('creating_table_from_view')
+    {%- endfor %}
+    {{ return(view_tmp_tables_mapping) }}
+{%- endmacro %}
+
+{% macro teradata__drop_tmp_tables_of_views(view_tmp_tables_mapping) -%}
+    {% for relation, temp_table in view_tmp_tables_mapping.items() %}
+        {% call statement('drop_existing_table', fetch_result=False) %}
+            DROP table /*+ IF EXISTS */ "{{ relation.schema }}"."{{ temp_table }}";
+        {% endcall %}
+        load_result('drop_existing_table')
+    {% endfor %}
+{%- endmacro %}
+
+
 --Decompose the existing get_catalog() macro in order to minimize redundancy with body of get_catalog_relations(). This introduces some additional macros
 {% macro teradata__get_catalog(information_schema, schemas) -%}
-    {% set use_qvci = var("use_qvci", "false") | as_bool %}
-    {{ log("use_qvci set to : " ~ use_qvci) }}
 
     {%- call statement('catalog', fetch_result=True) -%}
           with tables as (
@@ -12,28 +53,33 @@
               {{ teradata__get_catalog_columns_sql(information_schema) }}
               {{ teradata__get_catalog_schemas_where_clause_sql(schemas) }}
           )
-          {{ teradata__get_catalog_results_sql() }}
+          {{ teradata__get_catalog_results_sql(none) }}
     {%- endcall -%}
     {{ return(load_result('catalog').table) }}
 {%- endmacro %}
 
 --A new macro, get_catalog_relations() which accepts a list of relations, rather than a list of schemas.
 {% macro teradata__get_catalog_relations(information_schema, relations) -%}
-    {% set use_qvci = var("use_qvci", "false") | as_bool %}
-    {{ log("use_qvci set to : " ~ use_qvci) }}
-    
+    {% set view_relations = teradata__get_views_from_relations(relations) %}
+    {% set view_tmp_tables_mapping = teradata__create_tmp_tables_of_views(view_relations) %}
+
     {%- call statement('catalog', fetch_result=True) -%}
           with tables as (
               {{ teradata__get_catalog_tables_sql(information_schema) }}
-              {{ teradata__get_catalog_relations_where_clause_sql(relations) }}
+              {{ teradata__get_catalog_relations_where_clause_sql(relations, view_tmp_tables_mapping) }}
           ),
           columns as (
               {{ teradata__get_catalog_columns_sql(information_schema) }}
-              {{ teradata__get_catalog_relations_where_clause_sql(relations) }}
+              {{ teradata__get_catalog_relations_where_clause_sql(relations, view_tmp_tables_mapping) }}
           )
-          {{ teradata__get_catalog_results_sql() }}
+          {{ teradata__get_catalog_results_sql(view_tmp_tables_mapping) }}
     {%- endcall -%}
-    {{ return(load_result('catalog').table) }}
+
+    {% set catalog_table = load_result('catalog').table %}
+
+    {{ teradata__drop_tmp_tables_of_views(view_tmp_tables_mapping) }}
+
+    {{ return(catalog_table) }}
 {%- endmacro %}
 
 --get_catalog_tables_sql() copied straight from pre-existing get_catalog() everything you would normally fetch from DBC.tablesV
@@ -53,7 +99,6 @@
 
 --get_catalog_columns_sql() copied straight from pre-existing get_catalog() everything you would normally fetch from DBC.ColumnsJQV and DBC.ColumnsV
 {% macro teradata__get_catalog_columns_sql(information_schema) -%}
-    {% set use_qvci = var("use_qvci", "false") | as_bool %}
     SELECT
         NULL AS table_database,
         DatabaseName AS table_schema,
@@ -63,15 +108,10 @@
         ColumnID AS column_index,
         ColumnType AS column_type,
         CommentString AS column_comment
-		
-        {% if use_qvci == True -%}
-            FROM {{ information_schema_name(schema) }}.ColumnsJQV
-        {% else -%}
-            FROM {{ information_schema_name(schema) }}.ColumnsV
-        {% endif -%}
+        FROM {{ information_schema_name(schema) }}.ColumnsV
 {%- endmacro %}
 
-{% macro teradata__get_catalog_results_sql() -%}
+{% macro teradata__get_catalog_results_sql(view_tmp_tables_mapping) -%}
     ,
     columns_transformed AS (
         SELECT
@@ -134,8 +174,26 @@
       SELECT
           columns_transformed.table_database,
           columns_transformed.table_schema,
-          columns_transformed.table_name,
-          tables.table_type,
+          {% if view_tmp_tables_mapping is not none %}
+            CASE
+            {% for relation, temp_table in view_tmp_tables_mapping.items() %}
+                WHEN columns_transformed.table_name = '{{ temp_table }}' THEN '{{ relation.identifier }}'
+            {% endfor %}
+                ELSE columns_transformed.table_name
+            END AS table_name,
+          {% else %}
+            columns_transformed.table_name,
+          {% endif %}
+          {% if view_tmp_tables_mapping is not none %}
+            CASE
+            {% for relation, temp_table in view_tmp_tables_mapping.items() %}
+                WHEN columns_transformed.table_name = '{{ temp_table }}' THEN 'view'
+            {% endfor %}
+                ELSE tables.table_type
+            END AS table_type,
+          {% else %}
+            tables.table_type,
+          {% endif %}
           columns_transformed.table_comment,
           tables.table_owner,
           columns_transformed.column_name,
@@ -175,13 +233,22 @@
 --        3. else throw exception. Houston we do not have a something we can use.
     
 --        The result of the above is that dbt can, with "laser-precision" fetch metadata for only the relations it needs, rather than the superset of "all the relations in all the schemas in which dbt has relations".
-{% macro teradata__get_catalog_relations_where_clause_sql(relations) -%}
+{% macro teradata__get_catalog_relations_where_clause_sql(relations, view_tmp_tables_mapping) -%}
     where (
         {%- for relation in relations -%}
             {% if relation.schema and relation.identifier %}
                 (
                     upper("table_schema") = upper('{{ relation.schema }}')
-                    and upper("table_name") = upper('{{ relation.identifier }}')
+                    {% if view_tmp_tables_mapping is not none %}
+                        {% set temp_table = view_tmp_tables_mapping[relation] %}
+                        {% if not temp_table %}
+                            and upper("table_name") = upper('{{ relation.identifier }}')
+                        {% else %}
+                            and upper("table_name") = upper('{{ temp_table }}')
+                        {% endif %}
+                    {% else %}
+                        and upper("table_name") = upper('{{ relation.identifier }}')
+                    {% endif %}
                 )
             {% elif relation.schema %}
                 (
--- test/catalog/with_qvci/models/view_from_source.sql
+++
@@ -1,6 +0,0 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
-SELECT * FROM {{ source('alias_source_schema', 'alias_source_table') }}
