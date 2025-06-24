2ebd9db69bf574d4d883509d1ad4f3fa09a41fb3
vs255034 <vs255034@teradata.com>
2025-06-24 17:18:15 +0530

    changed external-tables macros directory

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ setup(
             'macros/*.sql',
             'macros/materializations/**/*.sql',
             'macros/utils/*.sql',
+            'macros/dbt_external_tables/helpers/*.sql',
+            'macros/dbt_external_tables/*.sql',
             'dbt_project.yml',
             'sample_profiles.yml',
         ],
