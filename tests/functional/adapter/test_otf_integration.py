20d2a024d88030bcc138c0ed6fed4d09f10f1e86
vs255034 <113147289+varun-sharma_teradata@users.noreply.github.com>
2026-06-10 15:38:31 +0530

    addressed copilot comments

--- tests/functional/adapter/test_otf_integration.py
+++ tests/functional/adapter/test_otf_integration.py
@@ -74,12 +74,9 @@ _OTF_TEST_TABLES = [
 def _cleanup_otf_tables(project):
     yield
     for name in _OTF_TEST_TABLES:
-        try:
-            project.run_sql(
-                f'DROP TABLE /*+ IF EXISTS */ "{DATALAKE_NAME}"."{OTF_DATABASE}"."{name}" NO PURGE;'
-            )
-        except Exception:
-            pass
+        project.run_sql(
+            f'DROP TABLE /*+ IF EXISTS */ "{DATALAKE_NAME}"."{OTF_DATABASE}"."{name}" NO PURGE;'
+        )
 
 
 CATALOG_NAME = "test_catalog"
